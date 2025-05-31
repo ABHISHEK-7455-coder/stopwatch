3281b94ddb6b8b4fc46bb87cb38f24e52b42fff1
Abhishek <abhishekkumardeshwal@gmail.com>
2025-05-31 23:30:36 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(0);
+  let timeRef = useRef(null)
+
+  function handleStart(){
+    timeRef.current = setInterval(() => {
+      setTime(time => time + 1)
+    }, 1000)
+  }
+
+  function handleStop(){
+    clearInterval(timeRef.current)
+  }
+
+  function handleReset(){
+    setTime(0)
+  }
+
+  return (
+    <>
+       <h1>Stopwatch : {time} seconds</h1>
+       <button onClick={handleStart}>START</button>
+       <button onClick={handleStop}>STOP</button>
+       <button onClick={handleReset}>RESET</button>
+    </>
+  )
+}
+
+export default App
